f979c9ee187f5571e47abf1fda945e0c32309e75
Ivochkina <romina23082008@gmail.com>
2020-05-26 15:25:56 +0200

    wk

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
 
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
 
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     });
@@ -23,9 +23,20 @@ function slideToggle(el) {
     elem.classList.toggle("open");
 }
 
-$('a[href^="#"]').click(function(){
+$('a[href^="#"]').click(function () {
     let anchor = $(this).attr('href');
     $('html, body').animate({
-        scrollTop:  $(anchor).offset().top
+        scrollTop: $(anchor).offset().top
     }, 6000);
 });
+// $(function(){ // сработает, когда документ загрузится
+//     $('#parent_popup').delay(2000) // пауза в 3 сек.
+//         .show(3000); // появление элемента в течении 1 сек.
+// });
+
+function explode(){
+$(document).ready(function() {
+    $("#myModal").modal('show');
+});
+}
+setTimeout(explode, 2000); //10000 это время, через которое нужно запустить функцию (1 секунд = 1000 миллисекунд)
